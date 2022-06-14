24dc63bf6f506e2d40a1623d42148138e72b8788
dangyt1719 <38418619+dangyt1719@users.noreply.github.com>
2022-06-14 17:10:42 +0300

    1st cmt to demo
    1st cmt

---
+++ WebApplication1/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Syncfusion.Blazor;
+
+namespace WebApplication1
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NTE2ODY0QDMxMzkyZTMzMmUzMEdFY1BzenZoOFkvbXUxRzBWZFVURWdROTFZTy9xOHo1WXFPSnprQnpwVFk9");
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.Services.AddSyncfusionBlazor();
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
